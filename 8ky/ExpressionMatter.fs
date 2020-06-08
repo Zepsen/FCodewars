f35b64b3e08ffd3c35eddd802121370129d53021
Дмитрий Зепсен <zepsendm@gmail.com>
2020-06-08 20:09:33 +0300

    style: add coments for last

--- 8ky/ExpressionMatter.fs
+++ 8ky/ExpressionMatter.fs
@@ -6,15 +6,18 @@ let multplus a b c = a * b + c
 let multPLUS a b c = a * (b + c)
 let multmult a b c = a * b * c
 
+// 1 with list
 let list = [plusplus; plusmult; PLUSmult; multplus; multPLUS; multmult;]
 
 let expressionMatter (a:int, b:int, c:int) =        
     List.map (fun f -> f a b c) list |> List.max
-    
+
+// 2 with list inside 
 let test a b c = 
     List.map (fun f -> f a b c) [plusplus; plusmult; PLUSmult; multplus; multPLUS; multmult]
     |> List.max
 
+// 3 without any additional function BEST!!!
 let best (a:int) (b:int) (c:int) : int =
     [a+b+c; a+b*c; (a+b)*c; a*b*c; a*b+c; a*(b+c) ] |> List.max
 
