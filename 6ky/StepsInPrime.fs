1e764db49662f43494d1f8f215825b987747ed87
Zepsen <zepsendm@gmail.com>
2020-07-03 20:21:15 +0300

    done

--- 6ky/StepsInPrime.fs
+++ 6ky/StepsInPrime.fs
@@ -1,27 +1,38 @@
-
 let isPrime (n:int64) =
-    [2L..n-1L]
-    |> List.forall (fun x -> n % x <> 0L) 
-
-let primesUpTo (m:int64) (n:int64): int64 list = 
-   let rec sieve l: int64 list  = 
-      match l with 
-      | [] -> []
-      | p::xs -> 
-            p :: sieve [for x in xs do if (x % p) > 0L then yield x]   
-   
-   [2L..n] |> sieve |> List.filter (fun x -> x > m)
-
-let takePrimes (m:int64) (n:int64): int64 list = 
-    [m..n]
-
-
+    let rec test (i:int64) = 
+        if i * i <= n then
+            if (n % i = 0L || n % (i + 2L) = 0L) then
+                false
+            else test (i + 6L)
+        else true    
+    
+    if n <= 3L then 
+        n > 1L
+    elif (n % 2L = 0L || n % 3L = 0L) then
+        false
+    else test 5L
 
 let step(g:int64, m:int64, n:int64): int64 list =
-    //let primes = [m..n] |> List.filter isPrime 
-    let primes = primesUpTo m n
+    let primes = [m..n] |> List.filter isPrime     
     match primes |> List.tryFind (fun x -> primes |> List.contains (x + g)) with
     | Some value -> [value; value + g]
     | None -> []
 
 step (6L, 100L, 108L)
+
+
+//Best variant 
+let isPrime n =
+    seq { for i in 2L..(n |> float |> sqrt |> int64) do yield n % i  }
+    |> Seq.tryFind (fun rem -> rem = 0L)
+    |> Option.isNone
+
+let step(g, m, n) =
+    let firstPrimeInStep = 
+        seq {m..n}
+        |> Seq.tryFind (fun i -> 
+            let upperNum = i + g
+            (i |> isPrime) && upperNum <= n && (upperNum |> isPrime))
+    match firstPrimeInStep with
+    | Some p -> [p; p + g]
+    | None -> []
