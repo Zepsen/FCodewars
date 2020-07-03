af4b9baaa051a290c081987f954c85d7cbfa8686
Dmytro Zepsen <Dmytro.Zepsen@refinitiv.com>
2020-07-03 19:04:56 +0300

    feat: prime method

--- 6ky/StepsInPrime.fs
+++ 6ky/StepsInPrime.fs
@@ -3,10 +3,25 @@ let isPrime (n:int64) =
     [2L..n-1L]
     |> List.forall (fun x -> n % x <> 0L) 
 
+let primesUpTo (m:int64) (n:int64): int64 list = 
+   let rec sieve l: int64 list  = 
+      match l with 
+      | [] -> []
+      | p::xs -> 
+            p :: sieve [for x in xs do if (x % p) > 0L then yield x]   
+   
+   [2L..n] |> sieve |> List.filter (fun x -> x > m)
+
+let takePrimes (m:int64) (n:int64): int64 list = 
+    [m..n]
+
+
+
 let step(g:int64, m:int64, n:int64): int64 list =
-    let primes = [m..n] |> List.filter isPrime 
+    //let primes = [m..n] |> List.filter isPrime 
+    let primes = primesUpTo m n
     match primes |> List.tryFind (fun x -> primes |> List.contains (x + g)) with
     | Some value -> [value; value + g]
     | None -> []
 
-step (2L, 2L, 22L)
+step (6L, 100L, 108L)
