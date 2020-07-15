7728156a98e971256102e9829dbe99c117fcafaa
Дмитрий Зепсен <zepsendm@gmail.com>
2020-07-15 14:02:56 +0300

    survivor: finish

--- 5ky/JosephusSurvivor.fs
+++ 5ky/JosephusSurvivor.fs
@@ -3,16 +3,21 @@ let josephusSurvivor n k =
     let rec go (arr:array<int>) from =
         match arr.Length with
         | 1 -> arr         
-        | _ -> if idx from < arr.Length then
-                    let innerArr = arr 
-                                   |> Array.mapi (fun i el -> (i <> idx from , el)) 
-                                   |> Array.filter fst 
-                                   |> Array.map snd                    
-                    go innerArr (idx from)
-               else 
+        | _ ->  let ix = idx from
+                if ix < arr.Length then
+                    let innerArr = Array.concat [arr.[..(ix-1)]; arr.[(ix+1)..]] 
+                    go innerArr ix
+                else 
                     go arr (from - arr.Length)     
     
     (go [|1..n|] 0).[0]
 
-josephusSurvivor 11 19 
+josephusSurvivor 300 300 
     
+
+let josephusSurvivorBest (n: int) (k: int) =
+  let rec josephusInternal(n: int, k: int) =
+    if n = 1 then 1
+    else (josephusInternal(n - 1, k) + k-1) % n + 1
+      
+  josephusInternal(n, k)
