1b0573fa1ecc3f48d93009c4db8a1814546da541
Zepsen <zepsendm@gmail.com>
2020-07-25 20:08:05 +0300

    feat: add simple

--- 5ky/BuddyPairs.fs
+++ 5ky/BuddyPairs.fs
@@ -1,10 +1,14 @@
-open System
-        
+let dis m = 
+    [1..m-1] 
+    |> List.filter (fun el -> m % el = 0) 
+    |> List.sum 
+
+let test n =
+    n + 1 = dis ((dis n) - 1)
+
 let buddy (start: int) (nd: int): (int * int) option = // your code
-    Some(0,0)
+    match [start..nd] |> List.tryFind test with
+    | Some res -> Some(res, dis res)
+    | _ -> None
 
-
-
-
-
-buddy 10 50
+buddy 1081130 1103735
