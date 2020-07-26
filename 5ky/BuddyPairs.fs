f1b8786889984c7a9114b2df351b162621c9d7a9
Zepsen <zepsendm@gmail.com>
2020-07-26 15:08:13 +0300

    feat: end my part

--- 5ky/BuddyPairs.fs
+++ 5ky/BuddyPairs.fs
@@ -6,12 +6,14 @@ let test n =
     let f1 = dis n
     if f1 > n then 
         let f2 = dis (f1 - 1)    
-        n + 1 = f2
-    else false
+        (n + 1 = f2, (f2 - 1, f1 - 1))
+    else (false, (0, 0))
 
 let buddy (start: int) (nd: int): (int * int) option = // your code
-    match [start..nd] |> List.tryFind test with
-    | Some res -> Some(res, dis res - 1)
+    match [start..nd] |> List.tryPick (fun el -> 
+            let (valid, res) = test el
+            if valid then Some(res) else None) with
+    | Some(x, y) -> Some(x, y)
     | _ -> None
 
 buddy 10 50
